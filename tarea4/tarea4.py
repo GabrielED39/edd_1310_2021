2c2b969b0c71513870389acc42ae3ad43453c319
GabrielED39 <72093413+GabrielED39@users.noreply.github.com>
2020-11-04 17:25:13 -0600

    Add files via upload

---
+++ tarea4/tarea4.py
@@ -0,0 +1,34 @@
+from arrays import Array
+class trabajador(Array):
+    def horase (self,suelb,ext):
+        return int(suelb) + (276.5 * init(ext))
+
+    def prestaciones(self,anoin,sueldo);
+    antigue = 2020 - ini(anoin)
+    presta = int(sueldo)*((3*antigue)/100)
+
+archivo = open('junio.dat','rt')
+archivo.readline()
+empleados = trabajador(14)
+for empleado in range(empleados.get_lenght()):
+    datos = archivo.readline().split(',')
+    empleados.set_item(datos,empleado)
+#Horas extra
+for empleado in empleados:
+    newsuel = empleados.horase(empleado[5], empleado[4])
+    empleado[5]=newsuel
+#Prestaciones
+for empleado in empleados:
+    empleado[5] = int(empleado[5])+empleados.prestaciones(empleado[6][0:4],empleado[5])
+for empleado in empleados:
+    print(empleado)
+#Antuguedad
+for x in range(empleados.get_length()):
+    for y in range(empleados.get_length()-1):
+        if empleados.get_item(y)[6][0:14] > empleados.get_item(y+1)[6][0:4]:
+            burbuja = empleados.get_item(y)
+            'empleados[y] = empleados[y+1]'
+            empleados.set_item(empleados.get_item(y+1),y)
+            'empleados[y+1] = burbuja'
+            empleados.set_item(burbuja,y+1)
+            print(empleados)
