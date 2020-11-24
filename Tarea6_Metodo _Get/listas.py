56b8493c90e7fe3b8c91c2dc2a925f2ab3d6c03a
GabrielED39 <72093413+GabrielED39@users.noreply.github.com>
2020-11-23 21:52:16 -0600

    Add files via upload

---
+++ Tarea6_Metodo _Get/listas.py
@@ -0,0 +1,63 @@
+
+class Nodo:
+    def __init__( self , value , siguiente= None):
+        self.data = value       # falta encapsulamiento
+        self.siguiente = siguiente
+
+#Metodo head (crea el nodo cabeza)
+class LinkedList:
+    def __init__( self ): # 
+        self.__head = None
+
+#Metodo empty (pregunta si esta vacio)
+    def is_empty( self ):
+        return self.__head == None
+
+#Metodo append (Agrega el nodo al final entrando por head.)
+    def append( self , value ):
+        nuevo = Nodo( value )
+        if self.__head == None: # self.is_empty()
+            self.__head = nuevo
+        else:
+            curr_node = self.__head
+            while curr_node.siguiente != None:
+                curr_node = curr_node.siguiente
+            curr_node.siguiente = nuevo
+            
+#Metodo transveral (Recorrido transversal para mostrar todos los elementos)
+    def transversal( self ):
+        curr_node = self.__head
+        while curr_node != None:
+            print(f" { curr_node.data } -> " , end="")
+            curr_node = curr_node.siguiente
+        print(" ")
+
+#Metodo remove (Elimina un nodo)
+    def remove(self, value):
+        curr_node = self.__head
+        if self.__head.data == value: # Este if cambia de nodo si se esta eliminando head
+            self.__head = self.__head.siguiente
+        else:
+            anterior = None
+            while curr_node.data != value and curr_node.siguiente != None:
+                anterior = curr_node #con esto se almacena el nodo anterior
+                curr_node = curr_node.siguiente
+            if curr_node.data == value:
+                anterior.siguiente = curr_node.siguiente
+            else:
+                print("El dato no existe en la lista")
+
+#Metodo preppend (Agrega un nuevo nodo al principio)
+    def preppend(self,value):
+        nuevo = Nodo(value, self.__head) #Crea el nuevo nodo
+        self.__head = nuevo #Le da al nuevo nodo el valor de head
+    
+    def get(self,posicion = None): #Por defecto regresa el ultimo
+        contador = 0
+        dato = None
+        if posicion == None:
+            dato = self.tail().data #Esto se convierte en un Nodo
+        else:
+            dato = self.preppend().data
+            contador+1
+        return dato
