efa76a7ae55ca59cbb10f21f560d631e42f5814d
GabrielED39 <72093413+GabrielED39@users.noreply.github.com>
2020-11-30 16:33:02 -0600

    Add files via upload

---
+++ Tarea8_ListaCircular/prueba_lista_circular.py
@@ -0,0 +1,11 @@
+from Lista_circular import ListaCircular
+m = ListaCircular
+
+print(f"Lista esta vacia ? { m.is_empty() } ")
+m.transversal
+m.insert(1)
+m.remove(1)
+m.insert(10)
+m.insert(15)
+m.insert(20)
+m.remove(10)
